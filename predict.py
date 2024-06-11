2a89d2f097a040179ea6fc1fa00ed9faa6b4c6fa
Sneha-Sivani-30 <167860637+Sneha-Sivani-30@users.noreply.github.com>
2024-06-11 19:53:25 +0530

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,13 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+%matplotlib inline
+df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%206/cbb.csv')
+df.head()
+df.shape
+df['windex'] = np.where(df.WAB > 7, 'True', 'False')
